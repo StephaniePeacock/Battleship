9d02cc6e63e6c5deaf3afa2104eac24f855b8b75
leo <hermosilloleoncio@gmail.com>
2024-03-31 22:11:51 -0700

    Not sure what keeps prompting path compilation on settins.json

--- aiGameplay/main.cpp
+++ aiGameplay/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     // Beginning of AI Gampeplay
     // First starting with "Dumb" AI
     // Then working with a smarter AI
+    cout << "Test" << endl;
     return 0;
 }
