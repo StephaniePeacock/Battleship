e023628a343e4c4ab330922ab1ec06876d3ddc00
hansintheair <hannesz1@gmail.com>
2024-05-08 17:22:22 -0700

    ignore build, dist, output, x64, .vscode folders

--- Accounts_Logins_Player/build/Debug/Cygwin-Windows/User.o.d
+++
@@ -1,9 +0,0 @@
-build/Debug/Cygwin-Windows/User.o: User.cpp User.h GamesDB.h Game.h \
- Generics.h Player.h Board.h Comp.h
-User.h:
-GamesDB.h:
-Game.h:
-Generics.h:
-Player.h:
-Board.h:
-Comp.h:
--- INTEGRATED/Accounts/build/Debug/Cygwin-Windows/Comp.o.d
+++
@@ -1,4 +0,0 @@
-build/Debug/Cygwin-Windows/Comp.o: Comp.cpp Comp.h Player.h Constants.h
-Comp.h:
-Player.h:
-Constants.h:
